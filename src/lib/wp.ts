c17dbcdcadcd0adba937f311629d047ad4ce0017
Angel Gala <galafloresangelemilio@gmail.com>
2025-06-24 12:49:05 -0500

    ....

--- src/lib/wp.ts
+++ src/lib/wp.ts
@@ -18,15 +18,13 @@ export const getPageInfo = async (slug: string) => {
 export const getLatestPost = async ({
   perPage = 10,
 }: { perPage?: number } = {}) => {
-  const response = await fetch(`${apiUrl}/posts?per_page=${perPage}`);
+  const response = await fetch(`${apiUrl}/posts?per_page=${perPage}&_embed`);
 
   if (!response.ok) throw new Error("Failed to fetch page info");
 
   const results = await response.json();
 
   if (results.length === 0) throw new Error("No posts found");
-
-  console.log(results);
 
   const posts = results.map((post) => {
     const {
@@ -37,8 +35,37 @@ export const getLatestPost = async ({
       slug,
     } = post;
 
-    return { title, excerpt, content, date, slug };
+    const featuredImage = post._embedded["wp:featuredmedia"][0].source_url;
+
+    return { title, excerpt, content, date, slug, featuredImage };
   });
 
   return posts;
+};
+
+export const getAllPostsSlugs = async () => {
+  const response = await fetch(`${apiUrl}/posts?per_page=100`);
+  if (!response.ok) throw new Error("Failed to fetch latest posts");
+
+  const results = await response.json();
+  if (!results.length) throw new Error("No posts found");
+
+  const slugs = results.map((post) => post.slug);
+  console.log(slugs);
+
+  return slugs;
+};
+
+export const getPostInfo = async (slug: string) => {
+  const response = await fetch(`${apiUrl}/posts?slug=${slug}`);
+
+  if (!response.ok) throw new Error("Failed to fetch page info");
+
+  const [data] = await response.json();
+  const {
+    title: { rendered: title },
+    content: { rendered: content },
+  } = data;
+
+  return { title, content };
 };
